bec7955f1f7fad53e63f96b432726996f47bdbaa
jacopo <jacopom@ifi.uio.no>
2017-06-19 10:33:22 +0200

    add test file script

---
+++ test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOG_FILE='/tmp/hyvar_test.log'
+
+python ../hyvar-rec.py sat.json > $LOG_FILE
+python ../hyvar-rec.py unsat.json >> $LOG_FILE
+python ../hyvar-rec.py --explain unsat.json >> $LOG_FILE
+python ../hyvar-rec.py --validate sat.json >> $LOG_FILE
+python ../hyvar-rec.py --validate unsat.json >> $LOG_FILE
+python ../hyvar-rec.py --check-interface interface.json sat.json >> $LOG_FILE
+python ../hyvar-rec.py --check-interface no_interface.json sat.json >> $LOG_FILE
+python ../hyvar-rec.py test1.json >> $LOG_FILE
+python ../hyvar-rec.py --features-as-boolean sat_bool.json >> $LOG_FILE
+
+diff $LOG_FILE output.txt
+rm $LOG_FILE
+
+
+
